6285fe22ef28560048fb4d0c5a0bc915812fa60b
rashmi0707 <64735988+rashmi0707@users.noreply.github.com>
2020-08-20 10:43:47 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,8 +1,9 @@
+$sITES=$SITES
 try{
         Install-Module -Name SharePointPnPPowerShellOnline -Force -Verbose -Scope CurrentUser  
 	Write-Output "Connecting on created, installing app on site..." 
         Write-Output $DEPLOYMENT_SCRIPT   
-	Write-Output $SITES
+	Write-Output $sITES
 	Write-Output "End of script"
 }
 catch{
