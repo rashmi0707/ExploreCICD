349e2cbf02fdd4ccaa6d1bbabe821d4bb00ccfe1
rashmi0707 <64735988+rashmi0707@users.noreply.github.com>
2020-08-20 10:39:45 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,6 +2,7 @@ try{
         Install-Module -Name SharePointPnPPowerShellOnline -Force -Verbose -Scope CurrentUser  
 	Write-Output "Connecting on created, installing app on site..." 
         Write-Output $DEPLOYMENT_SCRIPT   
+	Write-Output $SITES
 }
 catch{
     Write-Output "Error in Deploying App:  $_.Exception.Message"
