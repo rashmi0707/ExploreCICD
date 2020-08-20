bcd32a7ff7e19f8f2b384a2e60cabda11ae17803
rashmi0707 <64735988+rashmi0707@users.noreply.github.com>
2020-08-20 10:41:18 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -3,6 +3,7 @@ try{
 	Write-Output "Connecting on created, installing app on site..." 
         Write-Output $DEPLOYMENT_SCRIPT   
 	Write-Output $SITES
+	Write-Output "End of script"
 }
 catch{
     Write-Output "Error in Deploying App:  $_.Exception.Message"
